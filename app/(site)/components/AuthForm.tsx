fcde5502922fbf66656c098662771575f978f2fe
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-05-23 18:43:55 +0900

    Input route, auth groundwork laid out

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,8 +1,80 @@
 'use client';
 // Bad for server component testing due to high interactivity
+import { useCallback, useState } from "react";
+import { SubmitHandler, FieldValues, useForm } from "react-hook-form";
+// I don't know why vscode is erroring this out
+import Input from '@/app/components/inputs/Input';
+
+type Option = 'LOGIN' | 'REGISTER';
 
 export default function AuthForm() {
+    const [option, setOption] = useState<Option>('LOGIN'); 
+    const [loading, setLoading] = useState(false);
+
+    const toggleOption = useCallback(() => {
+        if (option === 'LOGIN') {
+            setOption('REGISTER');
+        } else {
+            setOption('LOGIN');
+        }
+    },[option]);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setLoading(true);
+
+        if (option === 'REGISTER') {
+            // fetch register
+        }
+        if (option === 'LOGIN') {
+            // NextAuth signin.  
+        }
+
+        function oAuthAction(action: string) {
+            setLoading(true);
+
+            //nextAuth socials signin
+        } 
+    }
+
     return (
-        <div>AuthForm Component Working</div>
+        <div
+            className="
+                mt-8
+                sm:mx-auto
+                sm:max-w-md
+            "
+        >
+            <div
+                className="
+                bg-zinc-800
+                px-4
+                py-8
+                shadow
+                sm:rounded-lg
+                sm:px-10
+            "
+            >
+                <form
+                    className='space-y-6'
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <Input />
+                </form>
+            </div>
+        </div>
     )
 }
