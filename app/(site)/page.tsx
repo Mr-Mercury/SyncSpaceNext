b5e75a9005aabf6b96033d425cc0775f04509093
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-05-23 17:34:17 +0900

    images - change later

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,8 +2,27 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-black-500 text-3xl">SyncSpace next!</p>
+    <div
+      className="
+      flex
+      min-h-full
+      flex-col
+      justify-center
+      py-12
+      sm:px-6
+      lg:px-8
+      bg-zinc-900
+      text-slate-50
+    ">
+      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+        <Image 
+          alt = 'logo'
+          height = '48'
+          width = '48'
+          className = 'mx-auto w-auto'
+          src = '/img/oslogo.png'
+        />
+      </div>
     </div>
   )
 }
