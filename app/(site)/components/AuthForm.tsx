135b746d72194ceadf8b633bf2585e82eb8f3209
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-05-24 15:53:16 +0900

    Auth Social button, divider created

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -5,6 +5,7 @@ import { SubmitHandler, FieldValues, useForm } from "react-hook-form";
 // I don't know why vscode is erroring this out
 import Input from '@/app/components/inputs/Input';
 import Button from '@/app/components/Button';
+import AuthSocialButton from "./AuthSocialButton";
 
 type Option = 'LOGIN' | 'REGISTER';
 
@@ -82,11 +83,36 @@ export default function AuthForm() {
                     <Input id='email' label='Email Address' type="email" register={register} errors={errors}/>
                     <Input id='password' label='Password' type="password" register={register} errors={errors}/>
                     <div>
-                        <Button>
+                        <Button
+                          disabled={loading}
+                          fullWidth
+                          type='submit'>
                             {option === 'LOGIN' ? 'Sign in' : 'Register'}
                         </Button>
                     </div>
                 </form>
+                <div className='mt-6'>
+                    <div className='relative'>
+                        <div
+                          className='
+                            absolute
+                            inset-0
+                            flex
+                            items-center'
+                        >
+                            <div className='w-full border-t border-gray-300' />
+                        </div>
+                        <div className='relative flex justify-center text-sm'>
+                            <span className='bg-slate-600 px-2 text-slate-50'>
+                                Or login with
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton />
+                    </div>
+                </div>
             </div>
         </div>
     )
---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,5 @@
+export default function AuthSocialButton() {
+    return (
+        <div>Social Button Working</div>
+    )
+}
