e93f9df43d64accfa6b044b0f891efb6b9fe6415
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-06-01 18:06:46 +0900

    Added tailwind config notes

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,18 @@
+const EmptyState = () => {
+    return(
+        <div 
+          className='
+            px-4
+            py-10
+            sm:px-6
+            lg:px-8
+            h-full
+            flex
+            justify-center
+            items-center
+            bg-zinc-800'
+        >Empty</div>
+    )
+}
+
+export default EmptyState;
